bcca638a512d82e50007857b6805a7db5b018b61
Purbayan Pramanik <purbayanpramanik62@gmail.com>
2026-04-06 12:37:34 +0530

    feat: add Homebrew casks (Ghostty, OBS, Docker) and Helium via mas

--- hosts/macbook/homebrew.nix
+++ hosts/macbook/homebrew.nix
@@ -1,12 +1,18 @@
 { ... }: {
-  homebrew = {
-    enable = true;
-    onActivation = {
-      autoUpdate = true;
-      cleanup = "zap";
-      upgrade = true;
+    homebrew = {
+        enable = true;
+        onActivation = {
+            autoUpdate = true;
+            cleanup = "zap";
+            upgrade = true;
+        };
+        casks = [
+            "ghostty"
+            "obs"
+            "docker"
+        ];
+        masApps = {
+            "Helium" = 1054607607;
+        };
     };
-    casks = [];
-    masApps = {};
-  };
 }
